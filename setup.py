c5cfa66b769f362acc151f8161737c1bd521861b
David McClure <davidwilliammcclure@gmail.com>
2017-10-01 16:10:00 -0400

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,15 @@
+
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='twitter-geo',
+    version='0.1.0',
+    description='Analysis rig for Twitter spatial data.',
+    url='https://github.com/davidmcclure/twitter-geo',
+    license='MIT',
+    author='David McClure',
+    author_email='dclure@mit.edu',
+    packages=find_packages(),
+)
