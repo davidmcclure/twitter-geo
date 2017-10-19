113cb96e35e635430ac4e5c3e10680c764fa9b4a
David McClure <davidwilliammcclure@gmail.com>
2017-10-19 12:24:49 -0400

    Distinctiveness.

--- twitter_geo/state_word_counts/db.py
+++ twitter_geo/state_word_counts/db.py
@@ -62,6 +62,25 @@ class StateWordCount(Base):
         return set([r[0] for r in query])
 
     @classmethod
+    def token_counts(cls, min_count=0):
+        """Get total (un-bucketed) token counts.
+
+        Args:
+            min_count (int)
+
+        Returns: OrderedDict
+        """
+        query = (
+            session
+            .query(cls.token, func.sum(cls.count))
+            .group_by(cls.token)
+            .having(func.sum(cls.count) > min_count)
+            .order_by(func.sum(cls.count).desc())
+        )
+
+        return OrderedDict(query.all())
+
+    @classmethod
     def state_count(cls, state):
         """Get the total count for a state.
 
